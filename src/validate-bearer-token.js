835e887e6dc8c68bbd81b105ec989267ab39aeeb
preet-singh <preetsingh13@gmail.com>
2019-09-30 16:15:57 -0500

    bookmarks.sql added - adds bookmarks to db

--- src/validate-bearer-token.js
+++ src/validate-bearer-token.js
@@ -4,6 +4,7 @@ const logger = require('./logger')
 function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN;
   const authToken = req.get('Authorization')
+  
   logger.error(`Unauthorized request to path: ${req.path}`)
   if (!authToken || authToken.split(' ')[1] !== apiToken) {
     return res.status(401).json({ error: 'Unauthorized request' })
